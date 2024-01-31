0ea29bdc6c792cc76ea36bddff05e52e89614256
jsmin6330 <jsmin6330@gmail.com>
2024-01-31 13:35:39 +0900

    basic v-for

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { ref } from 'vue'
 
-const message = ref('Hello World')
-const message_info = ref({name: "soomin", age: 29})
-
+const parentMessage = ref('Parent')
+const items = ref([{message: 'Foo'},{message:'Bar'}])
 </script>
 
 <template>
-	<h1>{{ message }}</h1>
-	<h1>{{ message_info }}</h1>
+	<li v-for = "(item, index) in items">
+		{{ parentMessage }} -  {{ index }} - {{ item.message }}
+	</li>
 </template>
