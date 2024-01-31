a87b8b859a20c5c7b6c63c7dd75b3e3bf613175a
jsmin6330 <jsmin6330@gmail.com>
2024-01-31 12:48:20 +0900

    add function

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup>
 import { ref } from 'vue'
 const awesome = ref(true)
+
+function toggleAwesome(){
+	isAwesome()
+}
+
+function isAwesome(){
+	awesome.value = !awesome.value
+}
+
 </script>
 
 <template>
-	<button @click = "awesome = !awesome">toggle</button>
+	<button @click = "toggleAwesome">toggle</button>
 
 	<h1 v-if="awesome">Vue is awesome!</h1>
 	<h1 v-else>Oh no 🥲</h1>
+
+	<h4 v-if="awesome">True</h4>
+	<h4 v-show="awesome">True</h4>
 </template>
