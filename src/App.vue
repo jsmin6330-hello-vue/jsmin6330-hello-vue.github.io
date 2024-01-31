de1dcbe5170a87cc9ec96540e8242e1d0f03a792
jsmin6330 <jsmin6330@gmail.com>
2024-01-31 11:16:51 +0900

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref,onMounted } from 'vue'
+
+//반응형 
+const count = ref(1)
+
+function increment(){
+	count.value = count.value + 1
+}
+
+onMounted(() => {
+	console.log(`The initial count is ${count.value}.`);
+})
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<div>
+	<button @click = "increment"> Count is: {{ count }}</button>
+	</div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+	button{
+		font-weight: bold;
+	}
 </style>
