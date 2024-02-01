c7745bfdad5e4513a7cea1620673b2b9d9ce864d
jsmin6330 <jsmin6330@gmail.com>
2024-02-01 11:21:12 +0900

    add lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
-import { ref, onUpdated } from 'vue'
+import { ref, onUpdated, onMounted, onBeforeUpdate } from 'vue'
 
 const count = ref(0)
+
+onMounted(() => {
+  console.log("Component mounted")
+})
+
+onBeforeUpdate(() => {
+  console.log("Component is about to update")
+})
+
 onUpdated(() => { 
 	console.log(document.getElementById('count').textContent)})
 
