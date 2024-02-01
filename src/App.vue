1b479a8edf5a167d7ce65267f84fc9122aa443ba
jsmin6330 <jsmin6330@gmail.com>
2024-02-01 21:44:29 +0900

    components
    - App.vue
    - ./components/Child.vue

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,7 @@
 <script setup>
-import { ref } from 'vue'
-
-let id = 0
-
-const newTodo = ref('')
-const todos = ref([
-	{ id: id++, issue: 'Backlog' } , { id: id++, issue: 'Todo'},
-	{ id: id++, issue: 'Going Hawaii'}
-])
-
-function addTodo(/*e*/){
-	//e.preventDefault();
-	todos.value.push({id: id++, issue: newTodo.value})	
-}
-
-
-function removeTodo(todo){
-	todos.value.splice(todo.index, 1)
-	//todos remove ...[].filter(t/f)
-	//todos.value = todos.value.filter((t) => t !== ?)
-}
-
+import child from './components/Child.vue'
 </script>
 
 <template>
-	<form @submit.prevent ="addTodo">
-		<input v-model = "newTodo">
-		<button>할 일 추가</button>
-	</form>
-	<ul>
-		<li v-for = "todo in todos">
-			{{ todo.issue }} - <button @click = "removeTodo(todo)" > Done </button>	
-		</li>
-	</ul>
+   <child/>
 </template>
-
-
-
---
+++ src/components/Child.vue
@@ -0,0 +1,3 @@
+<template>
+   <h2>자식 컴포넌트 입니다! </h2>
+</template>
