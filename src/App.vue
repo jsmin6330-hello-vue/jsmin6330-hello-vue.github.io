01899c09b3736d2888cf7ff28c355a27781c18aa
jsmin6330 <jsmin6330@gmail.com>
2024-02-01 11:15:51 +0900

    Add button click to update 'count' variable

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onUpdated } from 'vue'
 
-const message = ref('')
+const count = ref(0)
+onUpdated(() => { 
+	console.log(document.getElementById('count').textContent)})
+
 </script>
 
 <template>
-        <span>Muiltiline message is: </span>
-	<p style="white-space: pre-line;"> {{ message }} </p>
-	<textarea v-model = "message" placeholder = "add multiple lines" ></textarea>
+        <button id = "count" @click = "count++"> {{ count }} </button>
 </template>
+
