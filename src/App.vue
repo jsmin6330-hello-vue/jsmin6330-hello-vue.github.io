fd7de9b50292b4f8e217eef0a6bf88250d225046
jsmin6330 <jsmin6330@gmail.com>
2024-02-01 21:52:09 +0900

    components upgrade
    - ./App.vue
    - ./components/Child.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+import { ref } from 'vue'
 import child from './components/Child.vue'
+
+const pocketMoney = ref('부모 컴포넌트로부터 <✉️>을 전달 받았어요!')
 </script>
 
 <template>
-   <child/>
+	<!-- <child/> -->
+	<child :pocketMoney="pocketMoney" />
 </template>
