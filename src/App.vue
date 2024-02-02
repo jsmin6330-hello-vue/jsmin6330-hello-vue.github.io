efb02daf1cfffcb4ae418d1689250acf8392d500
jsmin6330 <jsmin6330@gmail.com>
2024-02-02 10:46:57 +0900

    morning_gymnastics
    - List 랜더링 On/Off
    - List 추가
    - List 제거
    - List 뒤집기

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <script setup>
 import { ref } from 'vue'
-import child from './components/Child.vue'
 
-const pocketMoney = ref('부모 컴포넌트로부터 <✉️>을 전달 받았어요!')
+const show = ref(true)
+const list = ref([1, 2, 3])
+let person = 4
+
+function onoff(){
+	show.value = !show.value
+}
+function addlist(){
+	list.value.push(person++)
+}
+function removelist(){
+	list.value.pop()
+}
+function reverselist(){
+	list.value.reverse()
+}
+
 </script>
 
 <template>
-	<!-- <child/> -->
-	<child :pocketMoney="pocketMoney" />
+        <button @click="onoff()">List 랜더링 On/Off </button>
+        <button @click="addlist()">List 추가 </button>
+        <button @click="removelist">List 제거 </button>
+        <button @click="reverselist()">List 뒤집기 </button>
+	
+	<template v-if="show">
+        <ul>
+                <li v-for="item of list"> {{ item }} </li>
+        </ul>
+	</template>
+        <p v-else-if= "list.length">List is not empty, but hidden.</p>
+        <p v-else>List is empty.</p>
 </template>
