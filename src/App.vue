2bdd5736a33a752b2d78d8755e85e01ab15d9614
jsmin6330 <jsmin6330@gmail.com>
2024-02-02 12:06:06 +0900

    원격 JSON 데이터를 가져와서 표시하는 프로그램
    "Fetch next todo" 버튼을 클릭하면 todoId가 증가하고
    해당 ID에 해당하는 할 일 데이터를 가져온다.
    데이터가 로드되면 JSON 형식으로 표시됩니다.

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,28 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
-const show = ref(true)
-const list = ref([1, 2, 3])
-let person = 4
+const todoId = ref(1);
+const todoData = ref(null)
 
-function onoff(){
-	show.value = !show.value
+async function fetchData(){
+	todoData.value = null
+	const res = await fetch(
+		`https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+	)
+	todoData.value = await res.json()
 }
-function addlist(){
-	list.value.push(person++)
-}
-function removelist(){
-	list.value.pop()
-}
-function reverselist(){
-	list.value.reverse()
+
+fetchData()
+function todoIdPuls(){
+	todoId.value++;
+	fetchData()
 }
 
 </script>
 
 <template>
-        <button @click="onoff()">List 랜더링 On/Off </button>
-        <button @click="addlist()">List 추가 </button>
-        <button @click="removelist">List 제거 </button>
-        <button @click="reverselist()">List 뒤집기 </button>
-	
-	<template v-if="show">
-        <ul>
-                <li v-for="item of list"> {{ item }} </li>
-        </ul>
-	</template>
-        <p v-else-if= "list.length">List is not empty, but hidden.</p>
-        <p v-else>List is empty.</p>
+	<p>Todo id: {{ todoId }} </p>
+	<button @click="todoIdPuls()" :disabled="!todoData">Fetch next todo</button>
+	<p v-if="!todoData">Loading...</p>
+	<pre v-else> {{ todoData }} </pre>
 </template>
